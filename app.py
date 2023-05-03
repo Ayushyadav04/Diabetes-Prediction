30e29b541f9a75a44744ea000675fa27db7a4921
Ayush yadav <113254994+Ayushyadav04@users.noreply.github.com>
2023-05-03 19:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from collections import Counter
-ds=pd.read_csv(r"C:\Users\AYUSH VIT\Downloads\diabetes.csv")
+ds=pd.read_csv(r"diabetes.csv")
 X = ds[ds.columns[:-1]].values
 y = ds['Outcome'].values
 x=ds.iloc[:,:-1].values
@@ -22,8 +22,8 @@ print("Accuracy for Random Forest Classifier is : ",accuracy_score(predictions,y
 
 
 import pickle
-pickle.dump(model,open(r"C:\Users\AYUSH VIT\Downloads\model_saved",'wb'))
-model_loaded=pickle.load(open(r"C:\Users\AYUSH VIT\Downloads\model_saved",'rb'))
+pickle.dump(model,open(r"model_saved",'wb'))
+model_loaded=pickle.load(open(r"model_saved",'rb'))
 model_loaded.predict(x_test)
 
 
@@ -33,8 +33,8 @@ model_loaded.predict(x_test)
 from flask import Flask, request,render_template
 import pickle
 import numpy as np
-app = Flask(__name__,template_folder=r'C:\Users\AYUSH VIT\Frontend tools\template',static_folder=r'C:\Users\AYUSH VIT\Frontend tools\static')
-model_loaded=pickle.load(open(r"C:\Users\AYUSH VIT\Downloads\model_saved",'rb'))
+app = Flask(__name__,template_folder=r'template',static_folder=r'static')
+model_loaded=pickle.load(open(r"model_saved",'rb'))
 @app.route('/')
 def home():
     return render_template('diabe.html')
